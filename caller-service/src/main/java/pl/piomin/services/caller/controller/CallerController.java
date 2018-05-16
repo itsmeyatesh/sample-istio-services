643468b504c66c2d5ededf432baa9ee47597cb5f
itsmeyatesh <36788342+itsmeyatesh@users.noreply.github.com>
2018-05-16 14:48:27 +0530

    To Test

--- caller-service/src/main/java/pl/piomin/services/caller/controller/CallerController.java
+++ caller-service/src/main/java/pl/piomin/services/caller/controller/CallerController.java
@@ -24,7 +24,7 @@ public class CallerController {
 	public String ping() {
 		LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), buildProperties.getVersion());
 		//String response = restTemplate.getForObject("http://callme-service:8091/callme/ping", String.class);
-		String response = restTemplate.getForObject("http://YBH/callme/ping", String.class);
+		String response = restTemplate.getForObject("http://callme-service/callme/ping", String.class);
 		LOGGER.info("Calling: response={}", response);
 		return buildProperties.getName() + ":" + buildProperties.getVersion() + ". Calling ... YBH ... " + response; 
 	}
